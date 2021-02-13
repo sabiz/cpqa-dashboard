4c2a8f68ad3d10dd333aac301047d3e0509b40bd
sAbIz <bellett@sabiz.jp>
2021-02-13 19:54:14 +0900

    Change request id

--- src/mut/request/coolantTemperature.ts
+++ src/mut/request/coolantTemperature.ts
@@ -3,12 +3,12 @@ import type MutRequest from './mutRequest';
 export default class CoolantTemperature implements MutRequest {
     readonly name: string = "CoolantTemperature";
     readonly nameShort: string = "CoolantTemp";
-    readonly requestId: number = 0x07;
+    readonly requestId: number = 0x10;
     readonly unitStr: string = "℃";
     readonly min: number = 0; // Normal 85~95
     readonly max: number = 190;
 
     eval(x: number): number {
-        return x;
+        return x-40;
     }
 }
